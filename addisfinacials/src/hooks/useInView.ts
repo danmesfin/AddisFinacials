e93f8f400f11b5db8fb19d528ac862b74e6fe42a
danmesfin <danielmsfn@gmail.com>
2024-08-16 22:23:19 +0300

    🍒 feat (main) added inview hook

---
+++ addisfinacials/src/hooks/useInView.ts
@@ -0,0 +1,28 @@
+import { useEffect, useState, useRef } from 'react';
+
+export function useInView() {
+  const [isInView, setIsInView] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsInView(entry.isIntersecting);
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    const currentRef = ref.current;
+    return () => {
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
+    };
+  }, []);
+
+  return [ref, isInView] as const;
+}
